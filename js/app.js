99f171c39532f66d3fae05334f7b49026848f6d1
Guadalupe97 <lumoranmtz@gmail.com>
2018-03-07 11:43:17 -0600

    Initial Commit

--- js/app.js
+++ js/app.js
@@ -10,17 +10,23 @@ app.controller('profesorCtrl', function ($scope) {
 	$scope.profesor = profesorData;
 	$scope.editando = {};
 
+	//variables para mostrar u ocultar
+
+	$scope.mostrar = false;
+
 	//definir función
 
 	$scope.editarprofesor = function(){
 		//hacer copia de alguna información y almacenarla en otro lugar 
 		// de donde lo va a copiar, donde lo va a guardar
 		angular.copy($scope.profesor, $scope.editando);
+		$scope.mostrar = true;
 	}
 
 	$scope.guardarcambios = function(){
 		// Se invierten los lugares
 		angular.copy($scope.editando, $scope.profesor);
+		$scope.mostrar = false;	
 	}
 
 	$scope.cancelar = function(){
@@ -28,6 +34,7 @@ app.controller('profesorCtrl', function ($scope) {
 		//arreglo vacío
 
 		$scope.editando = {};
+		$scope.mostrar = false;
 	}
 
 });
