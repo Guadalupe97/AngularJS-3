1dde99ec76084b068425637cd226175683eb6e79
Guadalupe97 <lumoranmtz@gmail.com>
2018-03-08 11:42:49 -0600

    Initial Commit

--- UniverApp/js/app.js
+++ UniverApp/js/app.js
@@ -2,6 +2,13 @@ var app = angular.module('universidadApp',['ngRoute']);
 
 app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
 
-  $scope.menuSuperior = "parciales/menu.html";
+  $scope.menuSuperior = 'parciales/menu.html';
 
+  $scope.setActive = function (Opcion) {
+      $scope.mInicio = "";
+      $scope.mProfesores = "";
+      $scope.mAlumnos = "";
+
+      $scope[Opcion] = "active";
+  }
 }]);
--- UniverApp/js/config.js
+++ UniverApp/js/config.js
@@ -3,13 +3,16 @@ app.config( function( $routeProvider ){
 
   $routeProvider
     .when('/',{
-        templateUrl: 'parciales/home.html'
+        templateUrl: 'parciales/home.html',
+        controller: 'inicioCtrl'
     })
     .when('/alumnos',{
-        templateUrl: 'parciales/alumnos.html'
+        templateUrl: 'parciales/alumnos.html',
+        controller: 'alumnosCtrl'
     })
     .when('/profesores',{
-        templateUrl: 'parciales/profesores.html'
+        templateUrl: 'parciales/profesores.html',
+        controller: 'profesoresCtrl'
     })
     .otherwise({
       redirectTo: '/'
